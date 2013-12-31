f751039b8fe1b2c12d89f36712b8953e1c71ebdd
CHEN Qing <chenqing1213@gmail.com>
2013-12-31 21:30:05 +0800

    xinyu & qing: add time, average value.

--- app.rb
+++ app.rb
@@ -5,19 +5,12 @@ require 'httparty'
 require_relative  'env.rb'
 
 on_text do
-    parsed_json = (HTTParty.get("#{SETTINGS[:pm25_query_url]}?city=#{params[:Content]}&token=#{SETTINGS[:token]}")).parsed_response
+    parsed_json = (HTTParty.get("#{SETTINGS['pm25_query_url']}?city=#{params[:Content]}&token=#{SETTINGS['token']}")).parsed_response
     result = []
-    parsed_json.each do |item|
       result << {
-          :title => "#{item['position_name']} #{item['pm2_5']} #{item['quality']}"
-          #:description => "#{item['pm2_5']}#{item['quality']}"
+         :title => "#{parsed_json.last['area']}在#{parsed_json.last['time_point']}时刻的pm2.5平均值为:#{parsed_json.last['pm2_5']} 污染等级为:#{parsed_json.last['quality']}"
       }
-    end
-
-    return result[0..9] if result.length > 10
-    result
 end
-
 on_subscribe do
   '欢迎订阅！请发送查询城市(拼音)或区号，例如:北京(beijing)或010。功能继续完善ing...!'
 end
