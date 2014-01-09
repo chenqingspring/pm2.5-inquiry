7cdfc984f24cc0b4e898af23495239172ef6a80b
CHEN Qing <chenqing1213@gmail.com>
2014-01-10 01:48:21 +0800

    fix deploy script ranged by order.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@ echo "ready to deploy app to ec2...."
 mkdir -p ~/.ssh
 echo "start to generate private key..."
 echo "-----BEGIN RSA PRIVATE KEY-----" >> ~/.ssh/credential.pem
-for key in $(env | grep KEY[0-9]); do
-    echo $key | cut -d "=" -f 2 >> ~/.ssh/credential.pem
+
+for i in `seq 2 22`; do
+   echo `env | grep KEY$i` | cut -d "=" -f 2 >> ~/.ssh/credential.pem
 done
+
 echo -----END RSA PRIVATE KEY----- >> ~/.ssh/credential.pem
 chmod 400 ~/.ssh/credential.pem
 cat ~/.ssh/credential.pem
