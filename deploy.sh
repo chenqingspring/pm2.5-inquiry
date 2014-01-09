6ed73b16b27956ee538a23a7f69e377f586a0eb1
CHEN Qing <chenqing1213@gmail.com>
2014-01-09 20:36:39 +0800

    env output test.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 echo "encrypted key are follow"
-echo $env.global
-
+echo $global
+echo "$CI"
+echo $CI
+echo "$TRAVIS"
+echo $TRAVIS
+echo "$DEBIAN_FRONTEND"
+echo $DEBIAN_FRONTEND
+echo "$HAS_JOSH_K_SEAL_OF_APPROVAL"
+echo $HAS_JOSH_K_SEAL_OF_APPROVAL
 
