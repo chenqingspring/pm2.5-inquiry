b32b983258cbf788d71651da04415d148ebeef2d
CHEN Qing <chenqing1213@gmail.com>
2014-01-09 23:34:35 +0800

    add a bunch of keys.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 echo "ready to deploy app to ec2...."
-echo $FOO
-echo "SOMEVAR is below"
-echo $SOMEVAR
-echo "MY_SECRET_ENV"
-echo $MY_SECRET_ENV
-
+echo $KEY1
+echo $KEY2
+echo $KEY3
+echo $KEY4
+echo $KEY5
+echo $KEY6
+echo $KEY7
+echo $KEY8
+echo "end of keys"
 bundle exec cap production deploy
