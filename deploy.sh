4d62046e9419cedd88e01db8970101d8ef90d977
CHEN Qing <chenqing1213@gmail.com>
2014-01-09 20:57:06 +0800

    add S3 Deployment with travis setup.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 echo "encrypted key are follow"
-echo $global
-echo "$CI"
-echo $CI
-echo "$TRAVIS"
-echo $TRAVIS
-echo "$DEBIAN_FRONTEND"
-echo $DEBIAN_FRONTEND
-echo "$HAS_JOSH_K_SEAL_OF_APPROVAL"
-echo $HAS_JOSH_K_SEAL_OF_APPROVAL
-
+echo AWSAccessKeyId
+echo $AWSAccessKeyId
+echo AWSSecretKey
+echo $AWSSecretKey
