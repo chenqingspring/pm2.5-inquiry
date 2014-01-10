594c1143d37da23f11bb4f16607c939c02d9c2bd
CHEN Qing <chenqing1213@gmail.com>
2014-01-10 23:15:00 +0800

    modify deploy shell script.

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,18 @@
-echo "ready to deploy app to ec2...."
+#!/bin/bash
 
 mkdir -p ~/.ssh
 
 echo "start to generate private key..."
 
 echo "-----BEGIN RSA PRIVATE KEY-----" >> ~/.ssh/credential.pem
-
 for i in `seq 2 22`; do
    eval echo \$KEY$i >> ~/.ssh/credential.pem
 done
-
 echo -----END RSA PRIVATE KEY----- >> ~/.ssh/credential.pem
-
 chmod 400 ~/.ssh/credential.pem
-
 echo "private key generated!"
 
+echo "ready to deploy app to ec2...."
 bundle exec cap production deploy
 
 rm -rf ~/.ssh/credential.pem
