36273b406e704240e8aa127e80113ab76f13b389
CHEN Qing <chenqing1213@gmail.com>
2014-01-15 09:31:08 +0800

    fix time format.

--- lib/time_helper.rb
+++ lib/time_helper.rb
@@ -2,10 +2,10 @@ module TimeHelper
   def self.time_format(time_string)
     time_info = DateTime.parse(time_string)
     year = time_info.year
-    month = time_info.month
-    day = time_info.day
-    hour = time_info.hour
-    min = time_info.min
+    month = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
+    day = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
+    hour = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
+    min = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
     "#{year}-#{month}-#{day} #{hour}:#{min}"
   end
 end
