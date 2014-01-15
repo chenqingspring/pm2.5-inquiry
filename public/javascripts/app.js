24ffb849ae0e58e9f30fb8243efc54041e57c908
CHEN Qing <chenqing1213@gmail.com>
2014-01-16 00:28:38 +0800

    add bottom10 cities

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(".btn-success").bind('click',function(){
         window.location.href = "/top10";
     })
+
+    $(".btn-danger").bind('click',function(){
+        window.location.href = "/bottom10";
+    })
 });
