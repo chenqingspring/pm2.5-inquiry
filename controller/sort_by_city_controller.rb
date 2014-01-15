12157dd949f3259a4f4eea664e967391d6ff9e1b
CHEN Qing <chenqing1213@gmail.com>
2014-01-16 02:15:14 +0800

    Revert "add bottom10 cities"
    This reverts commit 24ffb849ae0e58e9f30fb8243efc54041e57c908.

--- controller/sort_by_city_controller.rb
+++ controller/sort_by_city_controller.rb
@@ -16,22 +16,3 @@ get '/top10' do
 
   haml :top, :locals => { :cities =>results }
 end
-
-get '/bottom10' do
-
-  city_ranking = Pm25Data.where(:name => 'city_ranking').all.last[:city_ranking]
-
-  results=[]
-  city_ranking[city_ranking.length-11..city_ranking.length-1].each do |city|
-    results << {
-        :aqi => city[:aqi],
-        :area => city['area'],
-        :pm2_5 => city['pm2_5'],
-        :quality => city['quality']
-    } unless city['pm2_5'].to_int == 0 || city['aqi'].to_int == 0
-  end
-
-  results.sort! {|x,y| y[:pm2_5].to_i <=> x[:pm2_5].to_i }
-
-  haml :bottom, :locals => { :cities =>results }
-end
