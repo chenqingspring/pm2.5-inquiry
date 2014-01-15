09c8f73dba42b2f643e6848072e9fef1d1d691bc
CHEN Qing <chenqing1213@gmail.com>
2014-01-15 13:48:44 +0800

    change time format with strftime.

--- lib/time_helper.rb
+++ lib/time_helper.rb
@@ -1,11 +1,6 @@
 module TimeHelper
   def self.time_format(time_string)
     time_info = DateTime.parse(time_string)
-    year = time_info.year
-    month = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
-    day = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
-    hour = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
-    min = time_info.month.to_int >= 10 ? time_info.month : "0#{time_info.month}"
-    "#{year}-#{month}-#{day} #{hour}:#{min}"
+    time_info.strftime("%Y-%m-%d %H:%M")
   end
 end
