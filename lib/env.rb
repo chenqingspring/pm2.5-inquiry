c2fc2cdce8407aaad77327fa88e9e44cdc3125d3
CHEN Qing <chenqing1213@gmail.com>
2014-01-18 08:08:25 +0800

    change schedule time from 1h to 20s.

--- lib/env.rb
+++ lib/env.rb
@@ -22,7 +22,7 @@ SETTINGS = YAML.load_file "./config/#{ENV['RACK_ENV']}.yml"
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '1h', :first_in => 0 do
+scheduler.every '20s', :first_in => 0 do
   city_ranking_info = Pm25ApiHelper.city_ranking
   if city_ranking_info.length > 20
     Pm25Data.new( :name => 'city_ranking',
