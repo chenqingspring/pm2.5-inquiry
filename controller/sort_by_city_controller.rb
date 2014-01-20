09bc722e999d28091379f5c7c06d02e47ae92977
CHEN Qing <chenqing1213@gmail.com>
2014-01-20 22:51:13 +0800

    refactor code.

--- controller/sort_by_city_controller.rb
+++ controller/sort_by_city_controller.rb
@@ -1,15 +1,5 @@
 get '/top10' do
-
-  city_ranking_info = Pm25ApiHelper.city_ranking
-  city_ranking = Pm25Data.where(:name => 'city_ranking').all.last[:city_ranking]
-  if city_ranking_info.length > 20 && city_ranking_info.first['time_point'] != city_ranking.first['time_point']
-    Pm25Data.new( :name => 'city_ranking',
-                  :city_ranking => city_ranking_info
-    ).save
-  end
-
-  city_ranking = Pm25Data.where(:name => 'city_ranking').all.last[:city_ranking]
-
+  city_ranking = Pm25ApiHelper.retrieve_city_ranking_data
   results=[]
   city_ranking[0..9].each do |city|
     results << {
@@ -26,17 +16,7 @@ get '/top10' do
 end
 
 get '/bottom10' do
-
-  city_ranking_info = Pm25ApiHelper.city_ranking
-  city_ranking = Pm25Data.where(:name => 'city_ranking').all.last[:city_ranking]
-  if city_ranking_info.length > 20 && city_ranking_info.first['time_point'] != city_ranking.first['time_point']
-    Pm25Data.new( :name => 'city_ranking',
-                  :city_ranking => city_ranking_info
-    ).save
-  end
-
-  city_ranking = Pm25Data.where(:name => 'city_ranking').all.last[:city_ranking]
-
+  city_ranking = Pm25ApiHelper.retrieve_city_ranking_data
   results=[]
   city_ranking[city_ranking.length-11..city_ranking.length-1].each do |city|
     results << {
--- lib/pm25_api_helper.rb
+++ lib/pm25_api_helper.rb
@@ -3,7 +3,22 @@ module Pm25ApiHelper
     HTTParty.get(URI.encode("#{SETTINGS['pm25_query_url']}?city=#{city_name}&token=#{SETTINGS['token']}".strip).to_s).parsed_response
   end
 
+
+  def self.retrieve_city_ranking_data
+    city_ranking_info = city_ranking
+    city_ranking = Pm25Data.where(:name => 'city_ranking').all.last[:city_ranking]
+    if city_ranking_info.length > 20 && city_ranking_info.first['time_point'] != city_ranking.first['time_point']
+      Pm25Data.new(:name => 'city_ranking',
+                   :city_ranking => city_ranking_info
+      ).save
+    end
+
+    Pm25Data.where(:name => 'city_ranking').all.last[:city_ranking]
+  end
+
+  private
   def self.city_ranking
     HTTParty.get(URI.encode("#{SETTINGS['aqi_ranking_url']}?token=#{SETTINGS['token']}".strip).to_s).parsed_response
   end
+
 end
