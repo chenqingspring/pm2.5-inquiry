31eac93ad7100874726ed790e2fcb64c1925ee24
Chen Qing <chenqing1213@gmail.com>
2014-01-25 14:39:08 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,6 @@ done
 echo -----END RSA PRIVATE KEY----- >> ~/.ssh/credential.pem
 chmod 400 ~/.ssh/credential.pem
 
-cat ~/.ssh/credential.pem
 echo "private key generated!"
 
 echo "ready to deploy app to ec2...."
