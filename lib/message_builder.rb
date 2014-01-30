633908d942a6fc12084baae4f038ced2a731e76e
CHEN Qing <chenqing1213@gmail.com>
2014-01-30 20:32:22 +0800

    Revert "add aqi description"
    This reverts commit 3e5ab033216e377b8e3b53369e7298ce726b57dc.
    
    Revert "update city ranking data when viewing its page."
    
    This reverts commit c47b7cab0b4f32e850bc63ec239de0b6fef6c4d9.

--- lib/message_builder.rb
+++ lib/message_builder.rb
@@ -5,7 +5,7 @@ module MessageBuilder
     city_name = last_average_data['area']
     result << {
         :title => "查询城市:#{city_name}",
-        :description => "        空气质量指数(aqi): #{last_average_data['aqi']}\n\npm2.5平均值: #{last_average_data['pm2_5']}\n\npm10平均值: #{last_average_data['pm10']}\n\n主要污染: #{last_average_data['primary_pollutant']}\n\n污染等级: #{last_average_data['quality']}\n\n发布时间: #{update_time}",
+        :description => "        aqi指数: #{last_average_data['aqi']}\n\npm2.5平均值: #{last_average_data['pm2_5']}\n\npm10平均值: #{last_average_data['pm10']}\n\n主要污染: #{last_average_data['primary_pollutant']}\n\n污染等级: #{last_average_data['quality']}\n\n发布时间: #{update_time}",
         :picture_url => picture_url,
         :url => URI.encode("#{SETTINGS['app_url']}/zones/#{city_name}")
     }
