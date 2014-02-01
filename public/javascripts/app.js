1e6e1f9a12b3bc60b4b634bcd46583252c0ff2c4
CHEN Qing <chenqing1213@gmail.com>
2014-02-02 00:31:43 +0800

    add loading images.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $('.btn-success, .btn-primary').bind('click',function(){
         window.location.href = "/top10";
+        $("body").addClass("loading");
     });
 
     $('.btn-danger,.btn-info').bind('click',function(){
         window.location.href = "/bottom10";
+        $("body").addClass("loading");
     });
 });
