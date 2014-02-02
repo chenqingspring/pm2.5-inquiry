16c62373ec1de820c43a86b3c10b0a3b45f8be16
CHEN Qing <chenqing1213@gmail.com>
2014-02-02 08:44:59 +0800

    Revert "try to use rack instead of passenger."
    This reverts commit 67cb6aed2b523021ef7c900ecbf5fbee97adba58.
    
    Revert "change transparent to .5"
    
    This reverts commit fe0a1e678063494e0059b91820520362b00182d7.

--- lib/env.rb
+++ lib/env.rb
@@ -18,15 +18,3 @@ MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 MongoMapper.database = "pm25-inquiry"
 
 SETTINGS = YAML.load_file "./config/#{ENV['RACK_ENV']}.yml"
-
-scheduler = Rufus::Scheduler.new
-
-scheduler.every '600s', :first_in => 0 do
-  city_ranking_info = Pm25ApiHelper.city_ranking
-  if city_ranking_info.length > 20
-    Pm25Data.new( :name => 'city_ranking',
-                  :city_ranking => city_ranking_info
-    ).save
-    puts 'ranking data saved'
-  end
-end
