b9f844fc01d3ea16a2fdacac76be0f3cf62b6bcc
CHEN Qing <chenqing1213@gmail.com>
2014-02-04 21:40:51 +0800

    Revert "add adv"
    This reverts commit b207bdd845477f62b4b519b8b1d9ec12e866fc6b.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,10 +8,4 @@ $(document).ready(function(){
         window.location.href = "/sort/bottom10";
         $("body").addClass("loading");
     });
-
-    $('.adv.alert').bind('click',function(){
-        window.location.href = "http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fre.taobao.com%2Feauction%3Fe%3DKNsCNmdtU4%252FebLdhAWchHFLwwtNwYCxu%252FgRj6p39QSeLltG5xFicOSZqewpHPyZzKnUjVbM4Ba5znfjY15mPXDUTFd7eF2DBT3yc3VnZj2iB3ujUJI0OeA%253D%253D%26ptype%3D100010&k=e2e107a2b72ca1b1&c=un&b=alimm_0&p=mm_52243835_5524728_17004310";
-        $("body").addClass("loading");
-    });
-
 });
