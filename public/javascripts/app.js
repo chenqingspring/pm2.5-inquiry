a6624535b2263042430a8cf6ebc53ab1cbc9b67b
CHEN Qing <chenqing1213@gmail.com>
2014-02-27 21:27:52 +0800

    add loading text.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
     $('.btn-success, .btn-primary').bind('click',function(){
         window.location.href = "/sort/top10";
         $("body").addClass("loading");
+        $('.modal .center').text('正在计算排名，请稍后...');
     });
 
     $('.btn-danger,.btn-info').bind('click',function(){
         window.location.href = "/sort/bottom10";
         $("body").addClass("loading");
+        $('.modal .center').text('正在计算排名，请稍后...');
     });
 
     $('.home a').bind('click', function(){
