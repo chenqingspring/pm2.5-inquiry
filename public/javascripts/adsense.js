9d8537dcfb43f823e3120ed94783c5929a673d7b
CHEN Qing <chenqing1213@gmail.com>
2014-03-19 18:44:05 +0800

    Revert "insert after modal."
    This reverts commit 48b4427ad20920ba2c9934f3f640f0e1cd485474.
    
    Revert "render adv by js"
    
    This reverts commit aa1dd879dc4570bbe673d3f8b5dbfad647f6cbbd.

--- public/javascripts/adsense.js
+++ public/javascripts/adsense.js
@@ -1,17 +1,1 @@
-(function () {
-    var po = document.createElement('script');
-    po.type = 'text/javascript';
-    po.async = true;
-    po.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
-    var ad = document.createElement('ins');
-    ad.className = 'adsbygoogle';
-    ad.style = 'display:block';
-    ad.setAttribute('data-ad-client','ca-pub-5345696631493625');
-    ad.setAttribute('data-ad-slot','9049332797');
-    ad.setAttribute('data-ad-format','auto');
-    var footer = $(".modal");
-    footer.after(ad)
-    (adsbygoogle = window.adsbygoogle || []).push({});
-})();
+(adsbygoogle = window.adsbygoogle || []).push({});
