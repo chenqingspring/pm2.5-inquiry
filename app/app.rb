676b60b5e7b3cc76edcb7f7bd454f11de09ecdf7
CHEN Qing <chenqing1213@gmail.com>
2014-03-28 09:53:46 +0800

    Revert "refactor code & add integration tests."
    This reverts commit 3cc878bc33a981e5db922336570fcd2126be52d9.

--- app/app.rb
+++ app/app.rb
@@ -1,20 +1,25 @@
 require_relative '../lib/env'
 
 on_text do
-    parsed_json = Pm25ApiHelper.parsed_json_builder params[:Content]
+    parsed_json = parsed_json_builder params[:Content]
     if parsed_json.is_a?(Hash) && !parsed_json['error'].nil? || parsed_json.length == 0
-      return '抱歉！暂未提供该城市pm2.5数据！'
+      return "抱歉！暂未提供该城市pm2.5数据！"
     end
     update_time = TimeHelper.time_format(parsed_json.last['time_point'])
     MessageBuilder.text_image_message(parsed_json.last, update_time)
 end
 
 on_location do
-  city_name = Pm25Inquiry::GeoCodeHelper.geo_code_parser params[:Location_X],params[:Location_Y]
-  parsed_json = Pm25ApiHelper.parsed_json_builder city_name
-
+  geo_coder_result = HttpClient.get(URI.encode("#{SETTINGS['geo_coder_url']}?location=#{params[:Location_X]},#{params[:Location_Y]}&coord_type=gcj02&output=json".strip).to_s).parsed_response
+  geo_city_name = JSON.parse(geo_coder_result)['result']['addressComponent']['city']
+  /(.*)市/.match(geo_city_name)
+  if $1.nil?
+    parsed_json = parsed_json_builder geo_city_name
+  else
+    parsed_json = parsed_json_builder $1
+  end
   if parsed_json.is_a?(Hash) && !parsed_json['error'].nil? || parsed_json.length == 0
-    return '抱歉！暂未提供该城市pm2.5数据！'
+    return "抱歉！暂未提供该城市pm2.5数据！"
   end
   update_time = TimeHelper.time_format(parsed_json.last['time_point'])
   MessageBuilder.text_image_message(parsed_json.last, update_time)
@@ -30,3 +35,14 @@ on_unsubscribe do
 end
 
 token 'chenqingspring'
+
+def parsed_json_builder city_name
+  parsed_json = Pm25ApiHelper.update_city_info(city_name)
+  last_city_info = Pm25Data.where(:name => city_name).all.last
+  if parsed_json.is_a?(Array) && (last_city_info.nil? || parsed_json.last['time_point'] != last_city_info.city_data.last['time_point'])
+    Pm25Data.new( :name => city_name,
+                  :city_data => parsed_json
+    ).save
+  end
+  parsed_json
+end
