87ac599e4192e307cd44d49b1ace5ea6e8d8365c
CHEN Qing <chenqing1213@gmail.com>
2014-05-22 13:52:08 +0800

    fix js requriement bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,2 +1,2 @@
-require(["helper/jquery-2.0.3.min", "helper/app", "helper/search", "helper/google-analytics"], function() {
+require(["helper/jquery-2.0.3.min", "helper/app", "helper/redirect", "helper/adsense", "helper/search", "helper/google-analytics"], function() {
 });
