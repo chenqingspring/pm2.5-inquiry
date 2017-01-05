48cf00285460402360885ab9b9385d85f68bb5ba
CHEN Qing <chenqing@sevencent.com.cn>
2017-01-05 11:15:58 +0800

    add message

--- controller/sort_by_city_controller.rb
+++ controller/sort_by_city_controller.rb
@@ -4,13 +4,13 @@
     sorted_cities = eval("Pm25ApiHelper.#{page}_cities")
     update_time = TimeHelper.time_format(Time.now.to_s)
 
-    if sorted_cities.empty? || sorted_cities[0] != 'error'
+    if !sorted_cities.empty? && sorted_cities.size != 1 # [["error", "Sorry，您这个小时内的API请求次数用完了，休息一下吧！"]]
       sorted_cities.each do |city|
         results << {
             :area => city['area'],
             :pm2_5 => city['pm2_5'],
             :quality => city['quality']
-        } unless ( city['pm2_5'].to_int == 0 || city['aqi'].to_int == 0 )
+        } unless city['pm2_5'].to_int == 0 || city['aqi'].to_int == 0
       end
       update_time = TimeHelper.time_format(sorted_cities.first['time_point'])
     end
